f06081c368f1f6d9eb71f7ae8874c14a621db0d6
AHMED BOUSALEH <92605896+bousaleh50@users.noreply.github.com>
2022-08-19 22:28:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ let menu=document.getElementById("main-menu");
 function showMenu(){
 	menu.classList.toggle("is-active");
 }
+
+let menuItems=document.querySelectorAll("#main-menu>.menu-item>a");
+
+for (const item of menuItems) {
+	item.addEventListener("click",function(){
+		menu.classList.toggle("is-active");
+	});
+}
